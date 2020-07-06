ad840a48cc7178c7be95df16ebd8134d83f1f46b
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-07-06 16:19:31 -0400

    uncommented tawk.to app

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ import ItServices from './components/ItServices';
 import RepairServices from './components/RepairServices';
 
 export default function App() {
-    // //<!--Start of Tawk.to Script-->
-    // var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    // (function(){
-    // var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    // s1.async=true;
-    // s1.src='https://embed.tawk.to/5e75be618d24fc2265891178/default';
-    // s1.charset='UTF-8';
-    // s1.setAttribute('crossorigin','*');
-    // s0.parentNode.insertBefore(s1,s0);
-    // })();
-    // //<!--End of Tawk.to Script-->
+    //<!--Start of Tawk.to Script-->
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5e75be618d24fc2265891178/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    //<!--End of Tawk.to Script-->
 
   return (
     <div className="App">
